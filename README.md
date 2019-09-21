0dbefa09ee86eebfafa340983c9e1cdecee3e49a
Dave Brace <dave.brace@gmail.com>
2019-09-20 21:36:20 -0500

    Initial commit of resume

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dave Brace's Resume
+
+Regenerate the main css file:
+
+```
+yarn postcss -o main.css tailwind.css
+```
---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./**/*.html"],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
+
+module.exports = {
+  plugins: [require("tailwindcss"), require("autoprefixer"), purgecss]
+};
